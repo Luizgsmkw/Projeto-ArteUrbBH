6b6b5fa4ec0ad2987a28d85cf4e2458c3e9a0e6a
Luizgsmkw <luizgustavokw@gmail.com>
2022-05-24 21:06:46 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+function modalPrincipal() {
+    var mostrarModalPrincipal= new bootstrap.Modal(document.getElementById("modalPrincipal"));
+  
+    mostrarModalPrincipal.show();
+  }
+
+function modal1() {
+    var mostrarModal1= new bootstrap.Modal(document.getElementById("modal1"));
+  
+    mostrarModal1.show();
+  }
+
+  function modal2() {
+    var mostrarModal2= new bootstrap.Modal(document.getElementById("modal2"));
+  
+    mostrarModal2.show();
+  }
+
+  function modal3() {
+    var mostrarModal3= new bootstrap.Modal(document.getElementById("modal3"));
+  
+    mostrarModal3.show();
+  }
+
+  function modal4() {
+    var mostrarModal4= new bootstrap.Modal(document.getElementById("modal4"));
+  
+    mostrarModal4.show();
+  }
+
+  function modal5() {
+    var mostrarModal5= new bootstrap.Modal(document.getElementById("modal5"));
+  
+    mostrarModal5.show();
+  }
+
+  function modal6() {
+    var mostrarModal6= new bootstrap.Modal(document.getElementById("modal6"));
+  
+    mostrarModal6.show();
+  }
